cf9cea973bd191d5a4e97e1b5ce2453188a7c01d
Bongshim <bongshime@gmail.com>
2022-08-12 21:29:01 +0100

    fix: alignment

--- regexTask/regexEmailTask.js
+++ regexTask/regexEmailTask.js
@@ -1,11 +1,11 @@
 const emailValidator = (email, message) => {
-    if (message !== undefined) {
-        return alert(message);
-    }
-    
-    const pattern = /^([\w-])+@([\w-])+\.\w{2,4}$/gi;
-    
-    return pattern.test(email) == true
+  if (message !== undefined) {
+    return alert(message);
+  }
+
+  const pattern = /^([\w-])+@([\w-])+\.\w{2,4}$/gi;
+
+  return pattern.test(email) == true
     ? alert(email.replace(/.(?=\w*@)/gm, "*"))
     : emailValidator(email, "enter a valid email");
 };
