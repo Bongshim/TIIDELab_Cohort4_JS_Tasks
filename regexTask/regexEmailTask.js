7245e71f640dfa3e1570a9bb08a85730ccc87ff4
Bongshim <bongshime@gmail.com>
2022-08-13 01:06:39 +0100

    fix: added comments

--- regexTask/regexEmailTask.js
+++ regexTask/regexEmailTask.js
@@ -1,10 +1,12 @@
 const emailValidator = (email, message) => {
+  // messages are only passed recursively if there is an error
   if (message !== undefined) {
     return alert(message);
   }
 
   const pattern = /^([\w-])+@([\w-])+\.\w{2,4}$/gi;
 
+  // tests for pattern and either replace or passes an error message recursively
   return pattern.test(email) == true
     ? alert(email.replace(/.(?=\w*@)/gm, "*"))
     : emailValidator(email, "enter a valid email");
